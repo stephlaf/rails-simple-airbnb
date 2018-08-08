64c3906c798f546c327f1635a8287ff26ec96bd8
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-08 16:01:58 -0400

    Added gems, created flat model and flats controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/new'
+  get 'flats/create'
+  get 'flats/edit'
+  get 'flats/update'
+  get 'flats/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to:
+  resources :flats
 end
