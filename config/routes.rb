23c01e8683d9e042d8dc9e193dacaad46900e264
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-09 08:21:15 -0400

    Added a bit of css

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/new'
-  get 'flats/create'
-  get 'flats/edit'
-  get 'flats/update'
-  get 'flats/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to:
+  
+  root to: 'flats#index'
   resources :flats
 end
